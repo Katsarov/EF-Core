13286d9ec892a2d8bc6e08e6eecf5b7b73377882
Kalin Katsarov <k.l.katsarov@gmail.com>
2021-03-14 15:27:01 +0200

    Added new files

---
+++ 08.LINQ_Lab/LinqDemo/EfLinqDemoMusik/Program.cs
@@ -0,0 +1,107 @@
+﻿using EfLinqDemoMusik.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EfLinqDemoMusik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MusicXContext();
+            var songs1 = db.Songs
+                .Where(x => x.SongArtists.Count() == 1)
+                .OrderBy(x => x.Name)
+                .Select(x => new
+                {
+                    x.Name,
+                    Artist1 = x.SongArtists.FirstOrDefault().Artist.Name
+                })
+                .Skip(100)
+                .Take(10)
+                .ToList();
+
+            foreach (var song in songs1)
+            {
+                Console.WriteLine(song.Artist1 + " " + song.Name);
+            }
+
+
+
+
+
+            return;
+
+            // ---------------------- SelectMany -----------------------------------
+            var artistsSongs = db.Artists
+                .OrderBy(x => x.Name)
+                .SelectMany(x => x.SongArtists
+                    .Select(sa => x.Name + " - " + sa.Song.Name))
+                .ToList();
+
+                foreach (var song in artistsSongs)
+                {
+                    Console.WriteLine(song);
+                }
+            
+
+            return;
+
+
+            // --------------------- GroupBy ------------------------------
+            var groups = db.Artists
+                .GroupBy(x => x.Name.Substring(0, 1))
+                .Select(x => new
+                {
+                    FirstLetter = x.Key,
+                    Count = x.Count()
+                })
+                .ToList();
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.FirstLetter} {group.Count}");
+            }
+
+            return;
+
+
+
+            // ----------------- OrderBy --------------------------------
+            var artists = db.Artists
+                .OrderByDescending(x => x.SongArtists.Count())
+                .Select(x => new 
+                    { 
+                        x.Name, 
+                        Count = x.SongArtists.Count()
+                    })
+                .Take(10)
+                .ToList();
+
+            foreach (var artist in artists)
+            {
+                Console.WriteLine($"{artist.Name} -> {artist.Count}");
+                //Console.WriteLine(artist);
+            }
+
+
+            return;
+
+
+            // ----------------- Where -----------------------------
+            var songs = db.Songs
+                .Where(x => x.Source.Name == "User")
+                .ToList();
+
+            int i = 1;
+            foreach (var song in songs)
+            {
+                Console.WriteLine($" {i} {song.Name}");
+                i++;
+                
+            }
+
+        }
+    }
+}
---
+++ 08.LINQ_Lab/LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new List<Student>()
+            {
+                new Student{Name = "Niki", Marks = new List<int>{2,3,2,3,2}},
+                new Student{Name = "Stoyan", Marks = new List<int>{ 6,5,6,5,6}},
+                new Student{Name = "Nivo", Marks = new List<int>{ 6,6,6}}
+            };
+
+            var groups = collection
+                //.Where(x => x.Marks.Average() >= 5)
+                .Select(x => new StudentProjection
+                {
+                    Name = x.Name,
+                    NameInitial = x.Name.Substring(0, 1),
+                    AverageMarks = x.Marks.Average(),
+                })
+                .OrderByDescending(x => x.AverageMarks)
+                .GroupBy(x => x.NameInitial);
+
+            //var collection2 = collection.Where(x => x.Name.StartsWith("N"));
+
+            //var collection2 = collection.Where(Predicate);
+            foreach (var group in groups)
+            {
+                //Console.WriteLine($"{student.Name} > {student.NameInitial} = {student.AverageMarks}");
+                Console.WriteLine($"{group.Key} {group.Count()}");
+            }
+        }
+
+        //  ------------------- Стандартен метод -------------------
+        //static bool Predicate(Student student)
+        //{
+        //    if (student.Marks.Average() >= 5)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //  ------------------- Същата функция с предикат -------------------
+        //static bool Predicate(Student student)
+        //{
+        //    return student.Marks.Average() >= 5;
+        //}
+
+        // -----------------------------------------------------------------
+    }
+
+    class StudentProjection
+    {
+        public string Name { get; set; }
+
+        public string NameInitial { get; set; }
+
+        public double AverageMarks { get; set; }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+
+        public List<int> Marks { get; set; }
+    }
+}
